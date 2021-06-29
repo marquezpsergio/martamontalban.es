e27fed3b75d6b7118fbc87c336616d7817734df9
Sergio Márquez <marquezpsergio@gmail.com>
2021-06-29 19:04:42 +0200

    Revert "ADDED: HEADER, FOOTER y HOME con estilos."
    This reverts commit 6124ddd15e9fd049ef394a739415664076d2d4bf.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-// Components
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './core/components/home/home.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { AboutMeComponent } from './core/components/about-me/about-me.component';
 
-
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'about-me', component: AboutMeComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
-]
 @NgModule({
   declarations: [
-    AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    HomeComponent,
-    AboutMeComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes, {
-      onSameUrlNavigation: "reload",
-      anchorScrolling: 'enabled',
-      scrollPositionRestoration: 'enabled'
-    })
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
