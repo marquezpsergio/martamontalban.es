a53ee634289ede6eb4bfa3d8bb76f9edc9bc35b5
Sergio Márquez <marquezpsergio@gmail.com>
2021-07-12 10:55:29 +0200

    ADDED - Añadidas variables de entorno de firebase y cambios en los tamaños permitidos en el angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBoHBceZnD1KWIu020nMROjU_Oz1HJjNAU",
+    authDomain: "mamont-d3364.firebaseapp.com",
+    projectId: "mamont-d3364",
+    storageBucket: "mamont-d3364.appspot.com",
+    messagingSenderId: "737837806689",
+    appId: "1:737837806689:web:18dc9df76f52185b0be620",
+    measurementId: "G-XEXKSLX7F0"
+  }
 };
