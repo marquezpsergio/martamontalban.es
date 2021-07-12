3fe38df9d6fd8f45d7f8fd19a10734eb245880d9
Sergio Márquez <marquezpsergio@gmail.com>
2021-07-12 11:26:53 +0200

    GIT IGNORE: Se ignorarán los archivos de firebase y las imágenes. Además, se eliminan las variables de firebase del entorno para solamente tenerlas en local.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyBoHBceZnD1KWIu020nMROjU_Oz1HJjNAU",
-    authDomain: "mamont-d3364.firebaseapp.com",
-    projectId: "mamont-d3364",
-    storageBucket: "mamont-d3364.appspot.com",
-    messagingSenderId: "737837806689",
-    appId: "1:737837806689:web:18dc9df76f52185b0be620",
-    measurementId: "G-XEXKSLX7F0"
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBoHBceZnD1KWIu020nMROjU_Oz1HJjNAU",
-    authDomain: "mamont-d3364.firebaseapp.com",
-    projectId: "mamont-d3364",
-    storageBucket: "mamont-d3364.appspot.com",
-    messagingSenderId: "737837806689",
-    appId: "1:737837806689:web:18dc9df76f52185b0be620",
-    measurementId: "G-XEXKSLX7F0"
-  }
+  production: false
 };
 
 /*
